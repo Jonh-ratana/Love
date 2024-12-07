1e6b0766142db0bdf689ab7b02152420bf08e095
Jonh-ratana <151663503+Jonh-ratana@users.noreply.github.com>
2024-12-07 14:46:16 +0700

    Add files via upload

---
+++ test.js
@@ -0,0 +1,11 @@
+// let data = [1,3,5,3,6,11,8]
+// console.log(data[data.length - 1])
+
+const data =[
+    ['A' ,'hi','B' ,'C'],
+    ['B' ,'C','B' ,'C'],
+    ['D' ,'E','B' ,'C'],
+    ['F' ,'Z','B' ,'C']
+]
+
+console.log(data[data.length - 2][1])
